53fe6b29f96f889c7e5fbb391549f5b8e52cbb54
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-04-23 20:58:20 +0300

    Initial Commit

---
+++ SportGameApp/SportGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportGameAppApp.swift
+//  SportGameApp
+//
+//  Created by Роман on 23.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SportGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
