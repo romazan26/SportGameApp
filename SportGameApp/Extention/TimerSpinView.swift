838784b730ad02b3f589aef2532cfc5d58aff206
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-05-03 23:59:35 +0300

    No timer and no sounds

---
+++ SportGameApp/Extention/TimerSpinView.swift
@@ -0,0 +1,18 @@
+//
+//  TimerSpinView.swift
+//  SportGameApp
+//
+//  Created by Роман on 03.05.2024.
+//
+
+import SwiftUI
+
+struct TimerSpinView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerSpinView()
+}
